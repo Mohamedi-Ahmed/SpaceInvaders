db65507b4aeaa7d97c2b84caf19f9c6725446036
Ahmed_M <62441252+Mohamedi-Ahmed@users.noreply.github.com>
2023-12-15 14:21:33 +0100

    ajout etat victoire et défaite

--- SpaceInvaders/Game.cs
+++ SpaceInvaders/Game.cs
@@ -24,36 +24,150 @@ namespace SpaceInvaders
             throw new NotImplementedException();
         }
 
-
+        private void DrawEndScreen(Graphics graphics, string message)
+        {
+            // Afficher un message de fin de jeu (Victoire ou Défaite)
+            Font font = new Font("Arial", 16);
+            SizeF textSize = graphics.MeasureString(message, font);
+            PointF positionTexte = new PointF((Form.ActiveForm.Width - textSize.Width) / 2, (Form.ActiveForm.Height - textSize.Height) / 2);
+            graphics.DrawString(message, font, Brushes.White, positionTexte);
+        }
 
         public void Draw(Graphics graphics)
         {
-            if (state == GameState.Pause)
+            switch (state)
             {
-                string pauseText = "Pause";
-                Font pauseFont = new Font("Arial", 16);
-                SizeF textSize = graphics.MeasureString(pauseText, pauseFont);
+                case GameState.Play:
+                    var tempObjetsDuJeu = new List<GameObject>(gameInstance.ObjetsDuJeu);
 
-                // Calculer le point de départ pour centrer le texte
-                float x = (Form.ActiveForm.Width  - textSize.Width) / 2;
-                float y = (Form.ActiveForm.Height - textSize.Height) / 2;
+                    // Dessinez ici les éléments du jeu
+                    foreach (var objet in tempObjetsDuJeu)
+                    {
+                        objet.Draw(graphics, objet.ObjectWidth, objet.ObjectHeight);
+                    }
+                    break;
 
-                // Dessiner le texte centré
-                graphics.DrawString(pauseText, pauseFont, Brushes.White, new PointF(x, y));
+                case GameState.Pause:
+                    string pauseText = "Pause";
+                    Font pauseFont = new Font("Arial", 16);
+                    SizeF textSize = graphics.MeasureString(pauseText, pauseFont);
+
+                    // Calculer le point de départ pour centrer le texte
+                    float x = (Form.ActiveForm.Width - textSize.Width) / 2;
+                    float y = (Form.ActiveForm.Height - textSize.Height) / 2;
+
+                    // Dessiner le texte centré
+                    graphics.DrawString(pauseText, pauseFont, Brushes.White, new PointF(x, y));
+                    break;
+
+                case GameState.Win:
+                    DrawEndScreen(graphics, "Victoire !");
+                    break;
+
+                case GameState.Loose:
+                    DrawEndScreen(graphics, "Défaite !");
+                    break;
+            }
+        }
+
+        private bool isPauseKeyPressed = false;
+
+        public void Update(Keys key, Size screenSize)
+        {
+            bool pKeyPressedNow = key == Keys.P;
+
+            // Gestion de la pause
+            if (pKeyPressedNow && !isPauseKeyPressed)
+            {
+                if (state == GameState.Play)
+                {
+                    state = GameState.Pause;
+                }
+                else if (state == GameState.Pause)
+                {
+                    state = GameState.Play;
+                }
+                isPauseKeyPressed = true;
+            }
+            else if (!pKeyPressedNow)
+            {
+                isPauseKeyPressed = false;
             }
 
-            else if (state == GameState.Play)
+            // Mise à jour des objets du jeu seulement si le jeu n'est pas en pause
+            if (state == GameState.Play)
             {
                 var tempObjetsDuJeu = new List<GameObject>(gameInstance.ObjetsDuJeu);
 
-                // Dessinez ici les éléments du jeu
-                foreach (var objet in tempObjetsDuJeu)
+                foreach (var gameObject in tempObjetsDuJeu)
                 {
-                    objet.Draw(graphics, objet.ObjectWidth, objet.ObjectHeight);
+                    gameObject.Update(key, screenSize);
+                }
+
+                // Vérifier si le joueur est mort
+                if (!playerShip.IsAlive())
+                {
+                    state = GameState.Loose;
+                }
+                // Vérifier si tous les ennemis sont détruits
+                else if (enemies != null && !enemies.IsAlive())
+                {
+                    state = GameState.Win;
+                }
+                /*
+                // Vérifier si les ennemis ont atteint le niveau du joueur
+                else if (enemies.ReachedPlayerLevel(playerShip.Position.y))
+                {
+                    state = GameState.Loose;
+                }
+                */
+                // Gérer les collisions
+                HandleCollisions();
+
+                // Supprimer les objets détruits après les mises à jour
+                gameInstance.ObjetsDuJeu.RemoveWhere(objet => !objet.IsAlive());
+            }
+
+            // Gestion des états de victoire et de défaite
+            if (state == GameState.Win || state == GameState.Loose)
+            {
+                if (key == Keys.Space)
+                {
+                    // Implémentez cette méthode pour redémarrer le jeu
+                    //RestartGame(screenSize); 
                 }
             }
         }
 
+        private void HandleCollisions()
+        {
+            foreach (var missile in gameInstance.ObjetsDuJeu.OfType<Missile>())
+            {
+                // Collision avec le vaisseau du joueur
+                playerShip.Collision(missile);
+
+                // Collision avec les bunkers
+                foreach (var bunker in gameInstance.ObjetsDuJeu.OfType<Bunker>())
+                {
+                    bunker.Collision(missile);
+                }
+
+                // Collision avec les ennemis
+                foreach (var enemy in gameInstance.ObjetsDuJeu.OfType<EnemyBlock>())
+                {
+                    enemy.Collision(missile);
+                }
+            }
+        }
+
+        private void RestartGame(Size screenSize)
+        {
+            // Réinitialiser le jeu
+            // ...
+            state = GameState.Play;
+        }
+
+        /*
         public void Update(Keys key, Size screenSize)
         {
             if (key == Keys.P)
@@ -102,7 +216,7 @@ namespace SpaceInvaders
 
             }
         }
-
+        */
 
         public Game(int Width, int Height)
         {
